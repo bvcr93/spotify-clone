6e86d1c9ccdc5a986e076b5fe550396456135e4f
bvcr93 <darelbavcar1@gmail.com>
2023-06-17 17:57:03 +0200

    home layout, liked songs button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,6 +4,9 @@ import { RxCaretRight } from "react-icons/rx";
 import React from "react";
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 interface HeaderProps {
   children: React.ReactNode;
   className?: string;
@@ -16,12 +19,9 @@ export default function Header({ children, className }: HeaderProps) {
   };
   return (
     <div
-      className={twMerge(
-        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
-        className
-      )}
+      className={twMerge(`h-fit bg-gradient-to-b from-red-900 p-6`, className)}
     >
-      <div className="w-full mb-4 flex items-center justify-between">
+      <div className="w-full mb-4 flex items-center justify-between text-white">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
             onClick={() => router.back()}
@@ -36,7 +36,33 @@ export default function Header({ children, className }: HeaderProps) {
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className=" text-white rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black " size={20} />
+          </button>
+          <button className=" text-white rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black " size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                className="bg-transparent text-neutral-300 font-medium"
+                onClick={handleLogout}
+              >
+                Logout
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Login
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 }
